3a24c9f47260248a8b102c6ea336508073063c22
kikio110 <sahanaturrizki@gmail.com>
2025-01-03 12:18:36 +0700

    feat: Menambahkan CRU layanan pada admin

---
+++ app/Filament/Admin/Resources/LayananResource/Pages/CreateLayanan.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Admin\Resources\LayananResource\Pages;
+
+use App\Filament\Admin\Resources\LayananResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateLayanan extends CreateRecord
+{
+    protected static string $resource = LayananResource::class;
+
+    protected function getFormSchema(): array
+    {
+        return ArtikelResource::form($this->form)->getSchema();
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return static::getResource()::getUrl('index');
+    }
+}
---
+++ database/migrations/2025_01_03_040248_create_client_counselers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client_counselers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('email');
+            $table->string('no_hp');
+            $table->string('domisili');
+            $table->text('keluhan');
+            $table->foreignId('jenis_layanan')->constrained('layanans')->onDelete('cascade');
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client_counselers');
+    }
+};
